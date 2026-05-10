fb0aa39d399fde3be12550bd00bb743808d713b1
Tambe <clavecris16@gmail.com>
2026-05-10 18:09:09 +0530

    Update ass6.java

--- ass6.java
+++ ass6.java
@@ -1,38 +1,49 @@
-package ds_6;
+package muth;
 
-import java.time.chrono.IsoChronology;
 import java.util.Scanner;
 
-public class ass6 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Enter number of processes : ");
-		
-		int n = scanner.nextInt();
-		
-		int token = 0;
-		
-		while(true) {
-			System.out.println("Token is with process : "+ token);
-			System.out.println("Does process" + token + " wants tp enter in CS");
-			
-			
-			int choice = scanner.nextInt();
-			
-			if (choice == 1) {
-				System.out.println("Process "+ token + "is in Critical scetions");
-				System.out.println("Process "+ token + "is in exiting scetions");
-			}
-					
-			token = (token+1)%n;
-			System.out.println("Continue (0/1)");
-			
-			int cont = scanner.nextInt();
-			if(cont == 0) {
-				break;
-			}
-			
-		}
-	}
+public class muth {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of processes: ");
+        int n = scanner.nextInt();
+
+        int token = 0;
+
+        while (true) {
+
+            System.out.println("\n--------------------------------");
+            System.out.println("Token is with Process " + token);
+            System.out.print("Does Process " + token + " want to enter Critical Section? (1 = Yes, 0 = No): ");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+
+                System.out.println("Process " + token + " ENTERED Critical Section");
+
+                // Critical section execution simulation
+                System.out.println("Process " + token + " EXITED Critical Section");
+
+            } else {
+
+                System.out.println("Process " + token + " skipped Critical Section");
+            }
+
+            // Pass token to next process
+            token = (token + 1) % n;
+
+            System.out.print("\nContinue simulation? (1 = Yes, 0 = No): ");
+            int cont = scanner.nextInt();
+
+            if (cont == 0) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
 }
